ac2d463d748a6d466a6f4c1cab72d20c9c45238e
Ramsey Zeitoun <r.i.zeitoun@gmail.com>
2020-01-16 10:25:29 -0500

    Fixed test

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -5,4 +5,4 @@ import app
 class TestStringMethods(unittest.TestCase):
 
     def test_message(self):
-        self.assertEqual(app.home(), 'Hello, World!')
+        self.assertEqual(app.home(), 'Hello, World!!!')
